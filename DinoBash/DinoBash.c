8db654b0b76edabc246beb5140cd43085a1fec8f
Michael Bergstrom <xander.berg1@gmail.com>
2026-09-22 23:19:42 -0600

    Working Bash with one small cd bug in there

--- DinoBash/DinoBash.c
+++ DinoBash/DinoBash.c
@@ -1,5 +1,6 @@
 #include <sys/types.h>
 #include <stdio.h>
+#include <sys/wait.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
@@ -10,45 +11,19 @@ int main()
 	int buffer_size = 100;
 	int max_arg_amount = 2;
 	char command_args[max_arg_amount][buffer_size];
-	char *command_ptr = malloc(buffer_size * sizeof(char));
 	char command_str[buffer_size];
-	if(command_ptr == NULL) {
-		fprintf(stderr, "Malloc Failed\n");
-		return 1;
-	}
-	int space_count = 0;
-	for(int i = 0; i < buffer_size; i++) {
-		if(command_str[i] == ' ') {
-			space_count++;
-		}
-	}
 	while(1) {
 		char *cwd = getcwd(NULL, 0);
 		printf("%s$ ", cwd);
-		fgets(command_ptr, buffer_size, stdin);
-		strncpy(command_str, command_ptr, buffer_size);
-		char *command;
-		if(space_count == 0) {
-			command = strtok(command_str, "\0");
-		}
-		else {
-			char *command = strtok(command_str, " ");
-			space_count--;
-		}
-		if(command == "exit") {
+		fgets(command_str, buffer_size, stdin);
+		char *command = strtok(command_str, " \n");
+		if(strcmp(command, "exit") == 0) {
 			break;
 		}
 
 		int arg_count = 0;
 		for(int i = 0; i < max_arg_amount; i++) {
-			char *curr_arg;
-			if(space_count == 0) {
-				curr_arg = strtok(NULL, "\0");
-			}
-			else {
-				curr_arg = strtok(NULL, " ");
-				space_count--;
-			}
+			char *curr_arg = strtok(NULL, " \n");
 			if(curr_arg == NULL) {
 				break;
 			}
@@ -56,8 +31,27 @@ int main()
 			strncpy(command_args[i], curr_arg, buffer_size);
 		}
 
-		char route[buffer_size] = "/bin/";
+		char route[100] = "/bin/";
 		strncat(route, command, buffer_size - strlen(route) - 1);
+
+		if(strcmp(command, "cd") == 0) {
+			if(command_args[0][0] == '\0') {
+				char *home_dir = getenv("HOME");
+				if(home_dir == NULL) {
+					fprintf(stderr, "Error: HOME environment variable is not set.\n");
+				}
+				else if(chdir(home_dir) != 0) {
+					fprintf(stderr, "Failed to change to home directory.\n");
+				}
+			}
+			else {
+				if(chdir(command_args[0]) != 0) {
+					fprintf(stderr, "Directory %s cannot be found.\n", command_args[0]);
+				}
+			}
+
+			continue;
+		}
 		pid_t pid;
 		pid = fork();
 		if(pid < 0) {
@@ -65,23 +59,7 @@ int main()
 			return 1;
 		}
 		else if(pid == 0) {
-			if(command == "cd") {
-				if(command_args[0][0] == '\0') {
-					char *home_dir = getenv("HOME");
-					if(home_dir == NULL) {
-						fprintf(stderr, "Error: HOME environment variable is not set.\n");
-					}
-					if(chdir(home_dir) != 0) {
-						fprintf(stderr, "Failed to change to home directory.\n");
-					}
-				}
-				else {
-					if(chdir(command_args[0]) != 0) {
-						fprintf(stderr, "No %s directory exists\n", command_args[0]);
-					}
-				}
-			}
-			else if(arg_count == 0) {
+			if(arg_count == 0) {
 				execlp(route,command,NULL);
 			}
 			else if(arg_count == 1) {
@@ -95,8 +73,7 @@ int main()
 			wait(NULL);
 			printf("Child Complete\n");
 		}
-		command_ptr[0] = '\0';
 	}
-	free(command_ptr);
-	command_ptr = NULL;
+
+	return 0;
 }
